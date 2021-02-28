1b99c36696b31d43c1025dcb0eac57ae223bfdf2
darcyxian <darcyxianau@gmail.com>
2021-03-01 10:12:57 +1100

    bug in header fixed, half of the aboutMe page fished, especially got the button effect down

--- header.js
+++ header.js
@@ -1,23 +1,39 @@
 let turn = true;
-if(turn){
-    // hide svg and menu
-    toggleSvg();
-    toggleSvgMenu();
-    console.log("tooggle");
+let dropMenuStatus = false;
+// when users open a new page, app need to display correct content according to the screen width
+if(window.innerWidth <= 800){
+   // find the screen is small
+    turn = false;
 }
-$(document).ready(function () {
-    $(".headerSvg").click(function () {console.log("right")
-    toggleSvgMenu();
-} )})
 
-    function toggleMenu() { $(document).ready(function () {
+function toggleMenu() { $(document).ready(function () {
     $(".menu").slideToggle(1);});}
 
     function toggleSvg() { $(document).ready(function () {
     $(".headerSvg").slideToggle(1);});} 
 
-    function toggleSvgMenu() { $(document).ready(function () {
+    function toggleDropMenu() { $(document).ready(function () {
     $(".dropMenu").slideToggle(1);});} 
+    function hideDropMenu() { $(document).ready(function () {
+        $(".dropMenu").hide(1);});} 
+
+if(turn){
+    // hide svg and dropmenu
+    toggleSvg();
+    toggleDropMenu();
+    console.log("tooggle");
+}else{
+    // hide menu and dropmenu
+    toggleMenu();
+    toggleDropMenu();
+    
+}
+$(document).ready(function () {
+    $(".headerSvg").click(function () {
+    toggleDropMenu();
+} )})
+
+  
 
 function resize() {
 
@@ -26,10 +42,12 @@ function resize() {
    if(width<800 && turn){
        toggleMenu();
        toggleSvg();
+       hideDropMenu();
        turn = false;
    }
    // if screen back to more than 800px, header back to normal by toggle twice.
    if(width >= 800 && turn === false){
+      hideDropMenu()
       toggleMenu();
       toggleSvg();
       turn=true;
@@ -38,22 +56,22 @@ function resize() {
 }
 window.onresize = resize;
 
-// 锁定正在访问的网页标签变色
-// let url = window.location.pathname;
-// console.log( url);
-// // find the target  <a>  and add a class
-// $(".menu__link").each(function () {
-//     let urlFind = returnUrl(url);
-//     if($(this).attr("href") === urlFind){
-//       $(this).addClass("selected");
+//锁定正在访问的网页标签变色
+let url = window.location.pathname;
+console.log( url);
+// find the target  <a>  and add a class
+$(".menu__link").each(function () {
+    let urlFind = returnUrl(url);
+    if($(this).attr("href") === urlFind){
+      $(this).addClass("selected");
 
-//     }
-//   })
-//   // 把url 转换成href 一样的长度
-// function returnUrl(urlString) {
+    }
+  })
+  // 把url 转换成href 一样的长度
+function returnUrl(urlString) {
      
-//      let number = urlString.lastIndexOf("/");
-//      let a = urlString.substring(number);
-//      let result = "."+a;
-//       return result;
-//   }
+     let number = urlString.lastIndexOf("/");
+     let a = urlString.substring(number);
+     let result = "."+a;
+      return result;
+  }
