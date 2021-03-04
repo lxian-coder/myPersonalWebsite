4e5b37135a27109845492c15c44fd9da1b64d6a6
darcyxian <darcyxianau@gmail.com>
2021-03-04 16:45:39 +1100

    addjust the size of background picture

--- changeBackImg.js
+++ changeBackImg.js
@@ -1,15 +1,15 @@
-let inbig = true;
+// let inbig = true;
 
-function changeBackgroundImg(){
-    if(window.outerWidth < 900 && inbig ){
-        $(".homeContainer").css("background-img","./assets/homeBackgroundtext.html");
-        inbig = false;
-        console.log("change small one");
-    }
-    if(window.outerWidth >= 900 && inbig === false ){
-        $(".homeContainer").css("background-img","./assets/homeBackground.html");
-        inbig = true;
-        console.log("change big one");
-    }
-}
-window.onresize = changeBackgroundImg;
+// function changeBackgroundImg(){
+//     if(window.outerWidth < 900 && inbig ){
+//         $(".homeContainer").css("background-img","./assets/homeBackgroundtext.html");
+//         inbig = false;
+//         console.log("change small one");
+//     }
+//     if(window.outerWidth >= 900 && inbig === false ){
+//         $(".homeContainer").css("background-img","./assets/homeBackground.html");
+//         inbig = true;
+//         console.log("change big one");
+//     }
+// }
+// window.onresize = changeBackgroundImg;
